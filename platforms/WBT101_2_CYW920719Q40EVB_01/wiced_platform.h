d1048e1713c6da219808d8156c17de4d99e4b91a
Greg Landry <gjl@cypress.com>
2018-03-19 07:45:37 -0400

    Updates for supermux config, debugging, bluetooth config tool, etc.

--- platforms/WBT101_2_CYW920719Q40EVB_01/wiced_platform.h
+++ platforms/WBT101_2_CYW920719Q40EVB_01/wiced_platform.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, Cypress Semiconductor Corporation or a subsidiary of Cypress Semiconductor 
+ * Copyright 2018, Cypress Semiconductor Corporation or a subsidiary of Cypress Semiconductor 
  *  Corporation. All rights reserved. This software, including source code, documentation and  related 
  * materials ("Software"), is owned by Cypress Semiconductor  Corporation or one of its 
  *  subsidiaries ("Cypress") and is protected by and subject to worldwide patent protection  
@@ -28,20 +28,50 @@
 #include "wiced_hal_gpio.h"
 
 //! Number of LEDs available on the platform.
+// LED_1 and LED_2 are pre-configured, others are configurable via SuperMux
 typedef enum
 {
-    WICED_PLATFORM_LED_1,  //!< LED 1
-    WICED_PLATFORM_LED_2,  //!< LED 2
+    WICED_PLATFORM_LED_1,  //!< LED 1 (WICED_P26)
+    WICED_PLATFORM_LED_2,  //!< LED 2 (WICED_P28)
+    WICED_PLATFORM_LED_3,  //!< LED 3
+    WICED_PLATFORM_LED_4,  //!< LED 4
+    WICED_PLATFORM_LED_5,  //!< LED 5
+    WICED_PLATFORM_LED_6,  //!< LED 6
+    WICED_PLATFORM_LED_7,  //!< LED 7
+    WICED_PLATFORM_LED_8,  //!< LED 8
     WICED_PLATFORM_LED_MAX //!< Max LED for error check
 } wiced_platform_led_number_t;
 
 //! Number of Buttons available on the platform.
+// BUTTON is pre-configured, others are configurable via SuperMux
 typedef enum
 {
-    WICED_PLATFORM_BUTTON_1,  //!< BUTTON 1
-    WICED_PLATFORM_BUTTON_2,  //!< BUTTON 2
+    WICED_PLATFORM_BUTTON_1,  //!< BUTTON 1 (WICED_P00)
+    WICED_PLATFORM_BUTTON_2,  //!< BUTTON 2 (WICED_P01)
+    WICED_PLATFORM_BUTTON_3,  //!< BUTTON 3
+    WICED_PLATFORM_BUTTON_4,  //!< BUTTON 4
+    WICED_PLATFORM_BUTTON_5,  //!< BUTTON 5
+    WICED_PLATFORM_BUTTON_6,  //!< BUTTON 6
+    WICED_PLATFORM_BUTTON_7,  //!< BUTTON 7
+    WICED_PLATFORM_BUTTON_8,  //!< BUTTON 8
     WICED_PLATFORM_BUTTON_MAX //!< Max button for error check
 } wiced_platform_button_number_t;
+
+
+//! Number of GPIO available on the platform.
+// Configurable via SuperMux
+typedef enum
+{
+    WICED_PLATFORM_GPIO_1,  //!< GPIO 1
+    WICED_PLATFORM_GPIO_2,  //!< GPIO 2
+    WICED_PLATFORM_GPIO_3,  //!< GPIO 3
+    WICED_PLATFORM_GPIO_4,  //!< GPIO 4
+    WICED_PLATFORM_GPIO_5,  //!< GPIO 5
+    WICED_PLATFORM_GPIO_6,  //!< GPIO 6
+    WICED_PLATFORM_GPIO_7,  //!< GPIO 7
+    WICED_PLATFORM_GPIO_8,  //!< GPIO 8
+    WICED_PLATFORM_GPIO_MAX //!< Max GPIO for error check
+} wiced_platform_gpio_number_t;
 
 //! Possible interrupt configuration for platform buttons
 typedef enum
@@ -106,7 +136,19 @@ typedef struct
 }
 wiced_platform_button_config_t;
 
-/*! pins for buttons and LEDs on the shield */
+/**
+ * Configuration for platform GPIOs
+ */
+typedef struct
+{
+    wiced_bt_gpio_numbers_t* gpio; /**< WICED GPIO pin */
+    uint32_t config; /**< configuration like GPIO_PULL_DOWN,GPIO_PULL_UP etc., interrupt is configured through wiced_platform_register_button_callback(...) */
+    uint32_t default_state; /**< GPIO_PIN_OUTPUT_HIGH/GPIO_PIN_OUTPUT_LOW */
+}
+wiced_platform_gpio_config_t;
+
+
+/*! pins for buttons annd LEDs on the shield */
 #define WICED_GPIO_PIN_LED_1     WICED_P26
 #define WICED_GPIO_PIN_LED_2     WICED_P28
 #define WICED_GPIO_PIN_BUTTON_1  WICED_P00
@@ -114,6 +156,8 @@ wiced_platform_button_config_t;
 
 /*! configuration settings for button, x can be GPIO_EN_INT_RISING_EDGE or GPIO_EN_INT_FALLING_EDGE or GPIO_EN_INT_BOTH_EDGE */
 #define WICED_GPIO_BUTTON_SETTINGS(x)                       ( GPIO_INPUT_ENABLE | GPIO_PULL_UP | x )
+
+/*! pins for LEDs */
 
 /*! Max. supported baudrate by this platform */
 #define HCI_UART_MAX_BAUD       4000000
