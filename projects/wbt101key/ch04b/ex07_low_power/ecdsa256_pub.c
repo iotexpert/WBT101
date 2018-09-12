f1407c7be3e0e0ba57b5a346cf2deab55e30fb18
Greg Landry <gjl@cypress.com>
2018-09-11 21:13:27 -0400

    Major updates to manual and examples after meetings in MD. Final version for class to be held WW1838 in KY.

--- projects/wbt101key/ch04b/ex07_low_power/ecdsa256_pub.c
+++
@@ -1,9 +0,0 @@
-#include <bt_types.h>
-#include <p_256_ecc_pp.h>
-
-// public key
-Point ecdsa256_public_key = 
-{
-    { 0x61fc814b, 0x52132f13, 0xc375f2a0, 0xa0230805, 0xf9bfa5f7, 0x307f490e, 0x3352017e, 0x3479b804, }, 
-    { 0x661b0ee3, 0xdfa4d811, 0x3398b14e, 0xcef1b2bf, 0x495d2274, 0x3d6610a1, 0x6a0cf2bc, 0x353594c9, },
-};
